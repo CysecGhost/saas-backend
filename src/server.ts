ba8392aacbeea1f740ee400b5d30c7b0d835c3d2
CysecGhost <shahidwork58@gmail.com>
2026-03-28 13:17:34 +0500

    feat: misc chores

--- src/server.ts
+++ src/server.ts
@@ -30,6 +30,6 @@ app.use("/analytics", authMiddleware, analyticsRoutes);
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
